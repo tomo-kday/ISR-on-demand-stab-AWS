7f4bc8ff9bd9424be236e3db76f8d1d77fceb532
tomo-kday <tomo.takebuchi@kkday.com>
2023-09-14 14:25:33 +0900

    disable in-memory caching to use a shared network mount

--- next.config.js
+++ next.config.js
@@ -3,4 +3,8 @@ module.exports = {
   images: {
     domains: ['avatars.githubusercontent.com'],
   },
+  experimental: {
+    // Defaults to 50MB
+    isrMemoryCacheSize: 0, // cache size in bytes
+  },
 };
