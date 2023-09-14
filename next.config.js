2561cd4b871418e106f33287e3bd6a5bbfba5193
tomo-kday <tomo.takebuchi@kkday.com>
2023-09-14 16:26:35 +0900

    added asset prefix to use CDN in production

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   images: {
     domains: ['avatars.githubusercontent.com'],
@@ -7,4 +10,6 @@ module.exports = {
     // Defaults to 50MB
     isrMemoryCacheSize: 0, // cache size in bytes
   },
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd ? 'https://d2p8t30rkjz869.cloudfront.net' : undefined,
 };
