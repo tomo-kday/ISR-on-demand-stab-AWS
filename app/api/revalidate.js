2e87aa0ee74019b3004eca0e61e926dfb2cc59aa
tomo-kday <tomo.takebuchi@kkday.com>
2023-09-15 12:58:55 +0900

    api revalidate stab

--- app/api/revalidate.js
+++ app/api/revalidate.js
@@ -33,10 +33,11 @@ export default async function handleWebhook(req, res) {
 
     // issue opened or edited
     // comment created or edited
-    console.log('[Next.js] Revalidating /');
+    console.log('⚡️ ⚡️ ⚡️ [Next.js] Revalidating /');
+    console.log();
     await res.revalidate('/');
     if (issueNumber) {
-      console.log(`[Next.js] Revalidating /${issueNumber}`);
+      console.log(`⚡️ ⚡️ ⚡️ [Next.js] Revalidating /${issueNumber}`);
       await res.revalidate(`/${issueNumber}`);
     }
 
