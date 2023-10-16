92856006c8df2f88919e51dfb9b397c150086f11
tomo-kday <tomo.takebuchi@kkday.com>
2023-10-16 19:31:05 +0900

    feat: took out assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 // @ts-ignore
-const isProd = process.env.NODE_ENV === 'production'
+// const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   images: {
@@ -12,5 +12,5 @@ module.exports = {
     isrMemoryCacheSize: 0, // cache size in bytes
   },
   // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://d2p8t30rkjz869.cloudfront.net' : undefined,
+  // assetPrefix: isProd ? 'https://d2p8t30rkjz869.cloudfront.net' : undefined,
 };
